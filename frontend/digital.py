a96f2c06ee66b581361fa122c119e320345fefdd
itz00priya <emo708121@gmail.com>
2026-04-13 16:40:51 +0530

    delete one file

---
+++ frontend/digital.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import requests
+
+
+st.sidebar.header("User Login")
+# Persist login state
+if "token" not in st.session_state:
+    username = st.sidebar.text_input("Username")
+    password = st.sidebar.text_input("Password", type="password")
+    if st.sidebar.button("Login"):
+        response = requests.post(f"{API_URL}/token", data={"username": username, "password": password})
+        if response.status_code == 200:
+            st.session_state["token"] = response.json()["access_token"]
+            st.sidebar.success("Logged In!")
+            st.rerun()
+        else:
+            st.sidebar.error("Invalid Credentials")
+else:
+    st.sidebar.success("✅ Currently Logged In")
+    if st.sidebar.button("Logout"):
+        del st.session_state["token"]
+        st.rerun()
