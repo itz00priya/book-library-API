efe4f8a9d906834df045b3fff35b1ef0d7fe654d
itz00priya <emo708121@gmail.com>
2026-04-14 12:17:57 +0530

    changes

--- app/auth.py
+++ app/auth.py
@@ -16,9 +16,10 @@ def verify_token(token: str):
         return payload
     except JWTError:
         return None
+    
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
+
+
 
 def get_password_hash(password):
     return pwd_context.hash(password)
